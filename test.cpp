be1bbf92a6ffc58983cd3d2151793bbd4555288f
agnuss7 <36191058+agnuss7@users.noreply.github.com>
2018-02-06 21:29:54 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,55 +1,109 @@
 #include <iostream>
-#include<string>
+
+#include <string>
+
 #include "string.h"
+
 using namespace std;
 
+
+
 int main()
+
 {
+
     string var;
+
     cout<<"Koks jusu vardas?"<<endl;
+
     cin>>var;
+
     int n=var.length();
+
     char a[2];
+
     a[0]=var[n-2];
+
     a[1]=var[n-1];
+
     string sv;
+
     if(a[1]=='a'||a[1]=='e')
+
     {
+
         sv="Sveika, ";
+
     }
+
     else if(a[0]=='a')
+
     {
+
         var[n-1]='i';
+
         sv="Sveikas, ";
+
     }
+
     else
+
     {
+
         var[n-2]='a';
+
         var[n-1]='u';
+
         sv="Sveikas, ";
+
     }
+
     sv=sv+var+"!";
+
     n=sv.length();
-    for (int i=0,i<n+4,i++)
+
+    for (int i=0;i<n+4;i++)
+
     {
+
         cout<<"*";
+
     }
+
     cout<<"\n* ";
-    for(int i=0,i<n,i++)
+
+    for(int i=0;i<n;i++)
+
     {
+
         cout<<" ";
+
     }
+
     cout<<" *\n* ";
+
     cout<<sv<<" *";
+
     cout<<"\n* ";
-    for(int i=0,i<n,i++)
+
+    for(int i=0;i<n;i++)
+
     {
-        cout<<" ";
+
+        std::cout<<" ";
+
     }
-    cout<<"*\n";
-    for(int i=0,i<n+4,i++)
+
+    cout<<" *\n";
+
+    for(int i=0;i<n+4;i++)
+
     {
+
         cout<<"*";
+
     }
+
     return 0;
+
 }
